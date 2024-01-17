51cd8dbd84817d6ec4129a66388dcc0f67e41351
alec016 <78179790+alec016@users.noreply.github.com>
2024-01-17 11:41:19 +0100

    changed component management, using a component manager.

--- forge/src/main/java/es/degrassi/forge/init/entity/type/IDegrassiEntity.java
+++ forge/src/main/java/es/degrassi/forge/init/entity/type/IDegrassiEntity.java
@@ -1,9 +1,12 @@
 package es.degrassi.forge.init.entity.type;
 
+import es.degrassi.forge.init.gui.component.*;
 import net.minecraft.core.BlockPos;
 
 public interface IDegrassiEntity {
   BlockPos getBlockPos();
 
   void setChanged();
+
+  ComponentManager getManager();
 }
